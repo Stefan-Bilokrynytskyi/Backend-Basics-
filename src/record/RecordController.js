21483ecc6aca25d8182acec656cb1adcf52b104e
Stefan-Bilokrynytskyi <stb3002@gmail.com>
2023-11-07 12:57:52 +0200

    added record controller

---
+++ src/record/RecordController.js
@@ -0,0 +1,68 @@
+'use strict';
+import Record from './Record.js';
+import Data from '../Data/Data.js';
+
+export default class RecordController {
+  createRecord(req, res) {
+    try {
+      const { idUser, idCategory, date, sumOfExpense } = req.body;
+      const record = new Record(
+        Data.records.length + 1,
+        Number(idUser),
+        Number(idCategory),
+        date,
+        sumOfExpense
+      );
+      Data.records.push(record);
+      res.status(200).json(record);
+    } catch (error) {
+      console.error('Error', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  getRecord(req, res) {
+    try {
+      const { id } = req.params;
+      const data = Data.records.find((record) => record.id === Number(id));
+      res.status(200).json(data);
+    } catch (error) {
+      console.error('Error', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  deleteRecord(req, res) {
+    try {
+      const { id } = req.params;
+      const record = Data.records.find((record) => record.id === Number(id));
+      Data.records = Data.records.filter((record) => record.id !== Number(id));
+      res.status(200).json(record);
+    } catch (error) {
+      console.error('Error', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  getRecords(req, res) {
+    try {
+      const { user_id, category_id } = req.query;
+      console.log(user_id);
+      console.log(typeof user_id);
+      let records = Data.records;
+      console.log(typeof records[0].idUser);
+      if (user_id) {
+        records = records.filter((record) => record.idUser === Number(user_id));
+      }
+      if (category_id) {
+        records = records.filter(
+          (record) => record.idCategory === Number(category_id)
+        );
+      }
+      if (!user_id && !category_id) {
+        throw new Error('Missing query parameters');
+      }
+      res.status(200).json(records);
+    } catch (error) {
+      console.error('Error', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import express from 'express';
 import { format } from 'date-fns';
 import UserController from './users/UserController.js';
 import CategoryController from './category/CategoryController.js';
+import RecordController from './record/RecordController.js';
 
 let status = 'offline';
 
@@ -17,4 +18,8 @@ router.get('/category/:id', new CategoryController().getCategory);
 router.get('/categories', new CategoryController().getCategories);
 router.delete('/category/:id', new CategoryController().deleteCategory);
 router.post('/category', new CategoryController().createCategory);
+router.get('/record/:id', new RecordController().getRecord);
+router.delete('/record/:id', new RecordController().deleteRecord);
+router.post('/record', new RecordController().createRecord);
+router.get('/record', new RecordController().getRecords);
 export default router;
